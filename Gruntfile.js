1ee01f8644abea94324d212315c9abbb769c7c27
Forrest Wilson <forrest.wilson@me.com>
2018-01-29 15:04:18 +1300

    Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,74 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        
+        jshint: {
+            files: ["*.js", "js/app.js"],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        csslint: {
+            strict: {
+                options: {
+                    import: 2
+                },
+                src: ["css/style.css"]
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    "js/app.min.js": ["js/app.js"]
+                },
+                output: {
+                    quoteStyle: 2
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: "css/",
+                    src: ["*.css", "!*.min.css"],
+                    dest: "css/",
+                    ext: ".min.css"
+                }]
+            }
+        },
+        watch: {
+            css: {
+                files: ["css/style.css", "css/sass.css"],
+                tasks: ["csslint", "cssmin"]
+            },
+            js: {
+                files: ["js/app.js"],
+                tasks: ["jshint", "uglify"]
+            },
+            sass: {
+                files: ["sass/style.scss"],
+                tasks: ["sass"]
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    "css/sass.css": "sass/style.scss"
+                }
+            }
+        }
     });
+
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-csslint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-sass");
+
+    grunt.registerTask("default", ["jshint", "csslint"]);
+    grunt.registerTask("min", ["csslint", "cssmin", "jshint", "uglify"]);
+    grunt.registerTask("w", ["watch"]);
+    grunt.registerTask("compile", ["sass"]);
 };
--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    console.log("Hey I Work");
+    function multiply(num1, num2) {
+        var total = num1 * num2;
+        return total;
+    }
+
+    function add(peram1, peram2) {
+        var total = peram1 + peram2;
+        $("body").append("<div>" + total + "</div>");
+    }
+
+    add(multiply(2, 4), 5);
+    add(multiply(4, 4), multiply(1, 5));
 });
